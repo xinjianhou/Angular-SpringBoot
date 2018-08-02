058f1c4e03b1402c5d9b3115ebc469b2a3aa6b73
xinjianhou <houxinjian7@outlook.com>
2018-08-02 22:30:46 +0800

    remove blank

--- wechat/src/main/java/com/xinjian/wechat/config/WebSecurityConfig.java
+++ wechat/src/main/java/com/xinjian/wechat/config/WebSecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 import static org.springframework.http.HttpMethod.OPTIONS;
 
 
