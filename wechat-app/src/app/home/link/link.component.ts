9e79aa2031f4ac7b2ca59dd754591c4d4837406c
xinjianhou <houxinjian7@outlook.com>
2018-08-03 19:48:22 +0800

    Refresh .gitignore file added.

--- wechat-app/src/app/home/link/link.component.ts
+++ wechat-app/src/app/home/link/link.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NgbDatepickerConfig} from '@ng-bootstrap/ng-bootstrap';
+import {DateUtil} from '../../_utils';
 
 @Component({
   selector: 'app-link',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LinkComponent implements OnInit {
 
-  constructor() { }
+  linkForm: FormGroup;
 
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder, config: NgbDatepickerConfig) {
+    config.maxDate = DateUtil.formatDate(new Date(1990, 1, 1));
+    config.maxDate = DateUtil.formatDate(new Date(2099, 11, 31));
+    config.outsideDays = 'collapsed';
   }
 
+  ngOnInit() {
+    this.buildForm();
+    this.f.searchDate.setValue(DateUtil.formatDate());
+  }
+
+  buildForm(): void {
+    this.linkForm = this.formBuilder.group({
+        searchDate: ['', [Validators.required]]
+
+      }
+    );
+  }
+  get f() {
+    return this.linkForm.controls;
+  }
 }
